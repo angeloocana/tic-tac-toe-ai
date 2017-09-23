1432b90f7f34f2510459533dec87fc80dab6c5f9
Angelo Ocana <angeloocana@gmail.com>
2017-09-23 17:01:04 -0400

    Add es locale

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,11 +11,13 @@ import {addLocaleData, IntlProvider} from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import fr from 'react-intl/locale-data/fr';
 import pt from 'react-intl/locale-data/pt';
+import es from 'react-intl/locale-data/es';
 
 import 'intl';
 import 'intl/locale-data/jsonp/en';
 import 'intl/locale-data/jsonp/fr';
 import 'intl/locale-data/jsonp/pt';
+import 'intl/locale-data/jsonp/es';
 
 import {
   getLangs,
@@ -41,7 +43,7 @@ import {
 //   }, 5000);
 // }
 
-addLocaleData([...en, ...fr, ...pt]);
+addLocaleData([...en, ...fr, ...pt, ...es]);
 
 const Background = styled.div`
   background-color: ${props => props.theme.bg};
