db7d5ece92508004c73b3c12835e2285b684fd85
Angelo Ocana <angeloocana@gmail.com>
2017-09-27 19:50:49 -0400

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: '',
+        trackingId: 'UA-61019439-6',
       },
     },
     {
