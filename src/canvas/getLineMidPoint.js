7b6ef439635e6b1a71fb7504fe4028f768ee91cf
Angelo Ocana <angeloocana@gmail.com>
2017-10-22 10:35:13 -0400

    Improve getLineMidPoint readability

--- src/canvas/getLineMidPoint.js
+++ src/canvas/getLineMidPoint.js
@@ -5,6 +5,9 @@
  * @return {Function} (percentage) => Mid point [x, y]
  */
 const getLineMidPoint = (p1, p2) => (per) =>
-  [p1[0] + (p2[0] - p1[0]) * per, p1[1] + (p2[1] - p1[1]) * per];
+  [
+    p1[0] + ((p2[0] - p1[0]) * per),
+    p1[1] + ((p2[1] - p1[1]) * per)
+  ];
 
 export default getLineMidPoint;
