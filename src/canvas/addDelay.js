de0047994feeaa82f7e8efcef82222f5b16cbd34
Angelo Ocana <angeloocana@gmail.com>
2017-11-05 18:35:38 -0500

    Add Hindley-Milner signatures

--- src/canvas/addDelay.js
+++ src/canvas/addDelay.js
@@ -2,6 +2,7 @@ import { range } from 'ramda';
 
 /**
  * Get list of nulls
+ * @sig Number -> [Null]
  * @param {Number} nFrames number of frames
  * @return {[null]} array of nulls
  */
@@ -9,6 +10,7 @@ const getNulls = (nFrames) => range(0, nFrames).map(_ => null);
 
 /**
  * Add nFrames delay to the end of each line
+ * @sig Number -> [a] -> [a]
  * @param {*} nFrames number of frames
  * @return {Function} concat lineChunks and range of nulls
  */
