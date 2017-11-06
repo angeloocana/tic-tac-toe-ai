cda1bfa8cf9e8fdacf073fb4a797eb32643822be
Angelo Ocana <angeloocana@gmail.com>
2017-11-05 19:22:11 -0500

    Add Hindley-Milner signatures

--- src/ai/calculateBitPosition.js
+++ src/ai/calculateBitPosition.js
@@ -1,16 +1,18 @@
 import getXYFromIndex from './getXYFromIndex';
 
 /**
- * Calculate bit position
- * @param {*} x row from 0 to 2
- * @param {*} y col from 0 to 2
+ * Calculate bit position for X and Y
+ * @sig Number -> Number -> Number
+ * @param {Number} x row from 0 to 2
+ * @param {Number} y col from 0 to 2
  * @return {Number} bit position
  */
 const calculateBitPositionForXY = (x, y) => (x + y * 3) * 2;
 
 /**
- * Calculate bit position
- * @param {*} index position index
+ * Calculate bit position for Index
+ * @sig Number -> Number
+ * @param {Number} index position index
  * @return {Number} bit position
  */
 const calculateBitPosition = (index) => {
