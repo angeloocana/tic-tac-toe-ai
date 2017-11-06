dea9a5cc5dd72cc56e67c8dd0ed6539656f47fa5
Angelo Ocana <angeloocana@gmail.com>
2017-11-05 19:29:27 -0500

    Add JsDocs to dec2bin

--- src/ai/dec2bin.js
+++ src/ai/dec2bin.js
@@ -1,4 +1,9 @@
-// eslint-disable-next-line no-bitwise
-const dec2bin = dec => (dec >>> 0).toString(2);
+/**
+ * Converts Number to Binary string
+ * @sig Number -> String
+ * @param {Number} dec decimal number
+ * @return {String} Binary string
+ */
+const dec2bin = dec => (dec >>> 0).toString(2); // eslint-disable-line no-bitwise
 
 export default dec2bin;
